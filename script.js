70f6d9eded11e2c4dd185e17d1b2e0aa4de8cb89
jrme421 <jeromepchallet@gmail.com>
2024-03-23 18:29:14 +0900

    final commit

--- script.js
+++ script.js
@@ -40,32 +40,29 @@ const poll = {
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
   // This generates [0, 0, 0, 0]. More in the next section!
   answers: new Array(4).fill(0),
-  registerNewAnswer: function () {
-    let answer = parseInt(
-      prompt(this.question + '\n' + this.options.join('\n'))
-    );
-    if (Number.isInteger(answer) && answer >= 0 && answer <= 3) {
+  registerNewAnswer() {
+    let answer = Number(prompt(this.question + '\n' + this.options.join('\n')));
+    console.log('answer: ', answer);
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
       this.answers[answer]++;
-      console.log('poll array 2: ', poll.answers);
-    } else {
-      alert('please only a nb 0 to 3');
-    }
-  this.displayResults();
+
+    this.displayResults();
+    this.displayResults('string');
   },
 
-  displayResults: function (type) {
-    if (type === "array") {
-      console.log();
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
     } else {
-      console.log('Poll results are ' + );
+      console.log('Poll results are ' + this.answers.join(', '));
     }
   },
 };
-console.log('poll array 1: ', poll.answers);
 
 document
   .querySelector('.poll')
   .addEventListener('click', poll.registerNewAnswer.bind(poll));
 
-poll.displayResults('string');
-poll.displayResults('array');
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [(1, 5, 3, 9, 6, 1)] }, 'string');
